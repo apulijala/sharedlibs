2dd97266f6af9e86a0eb9e25546dc972512483c3
apulijala12 <arvind.pulijala@informa.com>
2022-10-16 08:19:13 +0100

    fix: adding welcome groovy file

---
+++ vars/welcome.groovy
@@ -0,0 +1,22 @@
+// 7:21
+
+def call() {
+
+        def resultado = new StringBuilder()
+        def error     = new StringBuilder()
+        def curlcommand = "curl -u apulijala12:ATBBYqUr2H6ebptZbcF4ntrbdwES01B578A9 https://api.bitbucket.org/2.0/repositories/informa-ge/informa-aem-commons.git/refs/branches?pagelen=10&page=7"
+        def comando = curlcommand.execute()
+        comando.consumeProcessOutput(resultado, error)
+        println(resultado.toString())
+        comando.waitForOrKill(3000) //(4)
+
+        // println(resultado.toString())
+        def jsonVals = new groovy.json.JsonSlurper().parseText(resultado.toString())
+        def branches = []
+        for (myval in jsonVals.values) {
+            branches.add(myval.name)
+        }
+        println(branches)
+
+}
+
